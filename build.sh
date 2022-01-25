8dd47e08ddfb6aae320a41e6b36e1ecd40b368ff
kyiselr <93725165+kyiselr@users.noreply.github.com>
2022-01-25 19:56:57 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 cd
 
 # Packages
-    yay -S --needed --noconfirm librewolf-bin xsecurelock chromium pulsemixer thunar terminator xorg-server xorg-xinit
+    yay -S --needed --noconfirm dmenu librewolf-bin xsecurelock chromium pulsemixer thunar terminator xorg-server xorg-xinit
 
 # Build dwm & st
     cd ~/suckless/dwm && sudo make clean install
