c084ca7295d2c9acc9bc885a0711d2e4cd050b92
kyiselr <93725165+kyiselr@users.noreply.github.com>
2022-02-08 19:56:57 +0100

    Update build4void.sh

--- build4void.sh
+++ build4void.sh
@@ -2,9 +2,10 @@ cd
 
 # Packages
     sudo xbps-install -S base-devel libX11-devel libXft-devel libXinerama-devel dmenu xsecurelock pulsemixer thunar acpi brightnessctl 
+    
 # Build dwm & st
-    cd ~/suckless/dwm && sudo make clean install
-    cd ~/suckless/st && sudo make clean install
+    cd ~/suckless/dwm4void && sudo make clean install
+    cd ~/suckless/st4void && sudo make clean install
 
 # Import .xinitrc
     mv ~/suckless/xinitrc ~/.xinitrc
