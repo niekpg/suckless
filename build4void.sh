e8c4e936368d05e0f0c31957c5e1172505efd6ab
kyiselr <93725165+kyiselr@users.noreply.github.com>
2022-02-08 20:03:53 +0100

    Update build4void.sh

--- build4void.sh
+++ build4void.sh
@@ -1,7 +1,10 @@
 cd
 
 # Packages
-    sudo xbps-install -S base-devel libX11-devel libXft-devel libXinerama-devel dmenu xsecurelock pulsemixer thunar acpi brightnessctl 
+    sudo xbps-install -Su 
+    sudo xbps-install void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree
+    sudo xbps-install -Su
+    sudo xbps-install nvidia nvidia-libs-32bit xorg xinit base-devel libX11-devel libXft-devel libXinerama-devel dmenu xsecurelock pulsemixer thunar acpi brightnessctl 
     
 # Build dwm & st
     cd ~/suckless/dwm4void && sudo make clean install
